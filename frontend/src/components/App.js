de62aad95e751741ad95c2f7042e432e5aed5a81
sadric5 <sadricgamer@gmail.com>
2022-07-05 14:10:58 -0600

    able to like and remove a like from a tweet

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,13 @@
 import {useState, useEffect} from 'react';
 import React from 'react';
 import axios from 'axios';
-import Display from './DisplayTweet';
 import {TweetArea} from './MyStyle';
-import "./App.module.css"
+import {didUserLikeDislikeTweet } from './Handler';
 
 
 const token = document.getElementsByName("csrfmiddlewaretoken")[0].value
 axios.defaults.headers.common["X-CSRFToken"] = token
+const userId = document.getElementById("user-id").innerHTML
 
 const App = ()=>{
     const url = "http://10.0.0.87:8000/tweets/"
@@ -21,13 +21,17 @@ const App = ()=>{
         const paramsData = {"created_by":document.getElementById("user-id").innerHTML, "text":newTweet}
         axios.post(url, paramsData)
         .then(res=>console.log(res))
-        .catch(e =>console.error(e));
-        setNewTweet("")
-        setRerederBol(!rerenderBol)
+        .catch(e =>console.error(e))
+        .finally(() =>{
+            setNewTweet("")
+            setRerederBol(!rerenderBol)
+        })
+
     }
 
     //  API call to get all the tweet
    const getTweets = () => {
+
     axios.get(url)
     .then(res =>{
         setData(res.data)
@@ -36,22 +40,60 @@ const App = ()=>{
    }
 
     //   API call to update the like data 
-    const likeHandler = (val, id) => {
+    const likeHandler = (item) => {
         const userId = document.getElementById("user-id").innerHTML
         const url = "http://10.0.0.87:8000/likes/"
-        const tweetId = id
+        const tweetId = item.id
         const paramsData = {"tweet": tweetId, "like_by": userId, "like":true}
         axios.post(url, paramsData)
         .then(response =>{
             console.log(response)
-            setRerederBol(!rerenderBol)
         })
+        .catch(error=>likeTweetFailed(error, item, url))
+        .finally(() => setRerederBol(!rerenderBol));
+    }
+    // If we already like tweet call this funtion is the .catch
+    const likeTweetFailed = (res, item, url) =>{
+        if(res.response.data.non_field_errors){
+            // return console.log(res.response.data.non_field_errors[0], id)
+            return removeLike(url, item)
+        }else{
+            return console.log("Error unidentified. Here was the response:", res)
+        }
+    }
+    // remove the a user like from a tweet
+    const removeLike =(url, item) => {
+        const id = item.like.filter(id =>id.like_by == userId)[0].id
+        axios.delete(url+id)
+        .then(res=> console.log(res.status))
         .catch(error=>console.log(error))
+        .finally(() => setRerederBol(!rerenderBol));
+
     }
 
-   useEffect(() =>{
-    getTweets()
-   }, [rerenderBol])
+    // useeffet to fetch all the tweeet
+    useEffect(() =>{
+        getTweets()
+       }, [rerenderBol])
+       
+    // style for like and comment section
+    const tweetComentAndOthersStyle = {fontSize:"20px", color:"none"}
+
+    const userLikeStyle = (data) =>{
+        const likeDislikeColor = didUserLikeDislikeTweet(data)
+        return {fontSize:"20px", color: likeDislikeColor.like}
+    } 
+    // Count the number of like on each tweet
+    const numberlike = (data) =>{
+        let numberLike = 0;
+        data.map(item=>item.like==true?numberLike+=1:"")
+        return numberLike
+    }
+ 
+    let readyToComment = false;
+
+    const hand = () =>{ readyToComment = !readyToComment}
+
     return (
         <>
         <div className='container' style={{backgroundColor:"#D3D3D3"}}>
@@ -88,11 +130,45 @@ const App = ()=>{
                            </form>
                         </div>
                     </div>
-                    {data?data.map(item => <Display key={item.id} tweet={item} likeHandle={likeHandle()}/>):console.log()}
-                    {/* <Master data={data} likeHandle={likeHandler}/> */}
-                    
-                </div>
 
+                    {/* Display each tweet and comment with all  he like that go with it. */}
+                    {data?data.map(item =>
+                    <div href="chart" className='row p-4 m-2' style={{backgroundColor:"#DDDAE2"}}>
+                        <div className='col-3' style={{backgroundColor:"#B4C1DB"}}>
+                            <i className="material-icons" style={{fontSize:"50px", color:"blue"}}>person</i>
+                        </div>
+                        {/* Tweet, comment, like display */}
+                        <div className='col-9'>
+                            <div className='row'>
+                                <div className='col-4 fw-bold'>
+                                    {item.username}
+                                </div>
+                                <div className='col-8'>
+                                    {item.publish_date}
+                                </div>
+                            </div>
+                            <div className='row p-2'> {item.text}</div>
+
+                            {/* comment, like and share row */}
+                            <div className='row mt-5'>
+                                <div className='col-4'>
+                                    <button onClick={()=>likeHandler(item)} id={item.id} className="fa fa-thumbs-o-up opacity-25 border-0" style={userLikeStyle(item.like)}>{numberlike(item.like)}</button>
+                                </div>
+                                <div className='col-4'>
+                                    <button className="far fa-comment-dots opacity-25 border-0" style={tweetComentAndOthersStyle}></button>
+                                    {/* To be review to show the textarea only when I click on the comment Icon */}
+                                    <textarea hidden maxLength={200} onChange={hand} name="tweet" className="form-control-plaintext border border-1 rounded-3" id="your-tweet" placeholder="Any comment?" style={{backgroundColor:"#D3D3D3", resize:"none", overflow:"hidden", outlineStyle:"none"}}></textarea>
+                                </div>
+                                <div className='col-4'>
+                                    <button className="fa fa-share-square-o opacity-25 border-0" style={tweetComentAndOthersStyle}></button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    ):console.log()
+                    } 
+                </div>
                 <div className='col-lg-3'>
                     <h3> Left Bar</h3>
                 </div>
@@ -103,8 +179,5 @@ const App = ()=>{
         </>
     )
 }
-
-
-// const Master = (props) => props.data?props.data.map(item => <Display key={item.id} tweet={item} likeHandle={likeHandler}/>):console.log()
 
 export default App;
--- frontend/src/components/DisplayTweet.js
+++ frontend/src/components/DisplayTweet.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 import styles from "./App.module.css"
-import {didUserLikeDislikeTweet } from './Handler';
+
 
 
 
@@ -12,26 +12,14 @@ const url = "http://10.0.0.87:8000/likes/"
 
 const Display = (props, {likeHandle}) =>{
 
-    const [renderUpdata, setRenderUpdate] = useState(false)
-    const linkStyle= {color:"black", textDecoration:'none'}
-    const tweetComentAndOthersStyle = {fontSize:"20px", color:"none"}
-
-    const likeDislikeColor = didUserLikeDislikeTweet(props.tweet.like);
-
-    const userLikeStyle = {fontSize:"20px", color: likeDislikeColor.like}
-    let numLike = 0;
-    let numDislike=0;
-    let readyToComment = false;
-
-    const hand = () =>{ readyToComment = !readyToComment}
     
-    props.tweet.like.map(item=>item.like==true?numLike+=1:numDislike+=1)
 
 
 
     return(
         <>
-       
+        <a href="chart" target="_blank" style={linkStyle} id="elementLink" hidden> It's hidden and can be remore any time </a>
+        
         </>
 
 )}
